0705ebd738a51bf1cdd82f3758eabc72bc47b866
Pankaj Kumar <pankajrajpoot415@gmail.com>
2025-06-20 15:02:47 +0530

    Update axios.js

--- job-tracker-frontend/src/api/axios.js
+++ job-tracker-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-tracker-backend-fyh9.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
